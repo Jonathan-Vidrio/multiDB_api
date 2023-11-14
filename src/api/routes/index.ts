8645795a248d9c221638558e0a2d3eac1f8655ea
Jonathan-Vidrio <133240323+Jonathan-Vidrio@users.noreply.github.com>
2023-11-13 19:49:48 -0600

    update routes read

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -5,7 +5,7 @@ const PATH_ROUTER = `${__dirname}`;
 const router = Router();
 
 readdirSync(PATH_ROUTER).forEach((file) => {
-    const fileName = file.endsWith(".ts") ? file.substring(0, file.length - 3) : file;
+    const fileName = file.split('.')[0] + '.' + file.split('.')[1];
     if (fileName !== 'index') {
         const routerName = fileName.substring(0, fileName.length - 6);
         router.use(`/${routerName}`, require(`./${fileName}`).default);
